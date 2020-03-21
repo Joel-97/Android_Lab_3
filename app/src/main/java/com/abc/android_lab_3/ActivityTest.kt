794530232894b92f46a5a5e6d81992090212e467
Joel-97 <gomez.joel.0709@gmail.com>
2020-03-21 17:44:20 -0600

    Recambio Kotlin

--- app/src/main/java/com/abc/android_lab_3/ActivityTest.kt
+++ app/src/main/java/com/abc/android_lab_3/ActivityTest.kt
@@ -1,12 +1,11 @@
 package com.abc.android_lab_3
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class ActivityTest : AppCompatActivity() {
 
-    @Override
-    protected fun onCreate(savedInstanceState: Bundle) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
     }
